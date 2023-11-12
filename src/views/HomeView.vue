177b4d8bfa1b3ae03ac8fddc03abce5f60d22dd7
Clarance Liberiste Ntwari <60586899+claranceliberi@users.noreply.github.com>
2023-11-12 22:19:28 +0200

    initial

---
+++ src/views/HomeView.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <!-- nav-section -->
+      <div class="navbar">
+        <div class="logo">
+          <span>Brainy Bunch</span>
+        </div>
+        <div class="search">
+          <ion-icon name="search"></ion-icon>
+          <ion-icon name="menu"></ion-icon>
+        </div>
+      </div>
+
+      <!-- images -->
+      <div class="images">
+        <div class="img img-1"></div>
+        <div class="img img-2"></div>
+        <div class="img img-3"></div>
+        <div class="img img-4"></div>
+        <div class="img img-5"></div>
+      </div>
+
+      <!-- title -->
+      <div class="title">
+        <span>Brainy Bunch</span>
+      </div>
+
+      <div class="side-icon">
+        <ion-icon name="arrow-dropleft-circle" class="icn"></ion-icon>
+        <ion-icon name="arrow-dropright-circle" class="icn"></ion-icon>
+      </div>
+
+      <div class="bottom-bar">
+        <div>ALU</div>
+        <div>2022</div>
+        <div>Cohort 5</div>
+      </div>
+
+      <!-- social media -->
+      <div class="media">
+        <ul>
+          <li> <ion-icon name="logo-instagram"></ion-icon> </li>
+        </ul>
+      </div>
+    </div>
+  </main>
+</template>
