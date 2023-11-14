8be1f0961d4d24a402305f9bbbf3cf526ed0908e
Clarance Liberiste Ntwari <60586899+claranceliberi@users.noreply.github.com>
2023-11-14 16:31:15 +0200

    feat: experimenting background images

--- src/views/ChallengesView.vue
+++ src/views/ChallengesView.vue
@@ -6,6 +6,25 @@ import NavBar from '@/components/NavBar.vue'
 <template>
   <main class="about">
         <!-- nav-section -->
-        <NavBar />
+        <!-- <NavBar /> -->
+
+        <div class=" flex items-center relative w-full h-[100vh]">
+            <div class="bg-[url(/img/challenges/chal-2.jpg)]  bg-cover bg-center w-full h-[100vh] absolute brightness-75">
+            </div>
+
+
+            <div class="flex z-10">
+            
+                <div class="challenge-description pl-10  max-w-xl space-y-4">
+                    <div class="line w-10 h-2 bg-white"></div>
+                    <div class="challenge-number text-slate-100"> Challenge 01 </div>
+                    <div class="challenge-title text-8xl font-bold capitalize text-white"> Track the missing track </div>
+                    <div class="challenge-solution text-slate-100">We have been able to produce the best solution to this challenge, do not worry how it gonna look like we have alot to care about</div>
+                </div>
+                <div class="challenges">
+                </div>
+            </div>
+
+        </div>
   </main>
 </template>
