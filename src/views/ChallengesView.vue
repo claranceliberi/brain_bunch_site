f0ec99d7f15a8bb5dec44db38ecdb30a226ba50c
Clarance Liberiste Ntwari <60586899+claranceliberi@users.noreply.github.com>
2023-11-19 15:12:20 +0200

    feat: animating title in transition

--- src/views/ChallengesView.vue
+++ src/views/ChallengesView.vue
@@ -1,5 +1,86 @@
 <script setup lang="ts">
 import NavBar from '@/components/NavBar.vue'
+import { ref, computed } from 'vue';
+import { gsap } from 'gsap'
+
+
+
+const challenges = ref([
+    {
+        id:'01',
+        number: 'Challenge 01',
+        title:'Track the missing track',
+        description:'We have been able to produce the best solution to this challenge, do not worry how it gonna look like we have alot to care about',
+        image: 'bg-[url(/img/challenges/chal-1.jpg)]'
+    },
+    {
+        id:'02',
+        number: 'Challenge 02',
+        title:'We are here for Good',
+        description:'We have been able to produce the best solution to this challenge, do not worry how it gonna look like we have alot to care about',
+        image: 'bg-[url(/img/challenges/chal-2.jpg)]'
+    },
+    {
+        id:'03',
+        number: 'Challenge 03',
+        title:'Dark appears in the absence of light',
+        description:'We have been able to produce the best solution to this challenge, do not worry how it gonna look like we have alot to care about',
+        image: 'bg-[url(/img/challenges/chal-3.jpg)]'
+    },
+    {
+        id: '04',
+        number: 'Challenge 04',
+        title:'Nothing heals past like time',
+        description:'We have been able to produce the best solution to this challenge, do not worry how it gonna look like we have alot to care about',
+        image: 'bg-[url(/img/challenges/chal-4.jpg)]'
+    }
+])
+const active = ref(challenges.value.length - 1)
+const activeChallenge = computed(() => challenges.value[active.value])
+
+
+function handleSlides(index: number) {
+    const activeChal = challenges.value[index]
+    challenges.value.splice(index, 1)
+    challenges.value.push(activeChal)
+}
+
+function startDescription(){
+    gsap.from(".challenge-description .line", {
+    duration:3,
+    delay:0.1,
+    opacity:0,
+    y:20,
+    ease:"expo.out",
+  })
+
+  gsap.from(".challenge-description .challenge-number", {
+    duration:3,
+    delay:0.1,
+    opacity:0,
+    y:20,
+    ease:"expo.out",
+  })
+
+  gsap.from(".challenge-description .challenge-title", {
+    duration:3,
+    delay:0.1,
+    opacity:0,
+    y:20,
+    ease:"expo.out",
+  })
+
+  gsap.from(".challenge-description .challenge-solution", {
+    duration:3,
+    delay:0.1,
+    opacity:0,
+    y:20,
+    ease:"expo.out",
+  })
+
+
+  console.log('ahaa')
+}
 
 </script>
 
@@ -8,20 +89,39 @@ import NavBar from '@/components/NavBar.vue'
         <!-- nav-section -->
         <!-- <NavBar /> -->
 
-        <div class=" flex items-center relative w-full h-[100vh]">
-            <div class="bg-[url(/img/challenges/chal-2.jpg)]  bg-cover bg-center w-full h-[100vh] absolute brightness-75">
+        <div class=" flex items-end relative w-full h-[100vh]">
+            <div class=" bg-cover bg-center w-full h-[100vh] absolute brightness-50 " :class="challenges[active].image">
             </div>
 
 
-            <div class="flex z-10">
+            <div class="flex justify-stretch z-10 pt-10 relative w-full pb-32">
             
-                <div class="challenge-description pl-10  max-w-xl space-y-4">
-                    <div class="line w-10 h-2 bg-white"></div>
-                    <div class="challenge-number text-slate-100"> Challenge 01 </div>
-                    <div class="challenge-title text-8xl font-bold capitalize text-white"> Track the missing track </div>
-                    <div class="challenge-solution text-slate-100">We have been able to produce the best solution to this challenge, do not worry how it gonna look like we have alot to care about</div>
-                </div>
-                <div class="challenges">
+               <TransitionGroup @enter="startDescription" :css="false" appear>
+                <template v-for="(challenge,i) in challenges" :key="challenge.id">
+                    <div class="challenge-description pl-10  max-w-xl space-y-4 " v-if="i === active" >
+                        <div class="line w-10 h-2 bg-white"></div>
+                        <div class="challenge-number text-slate-100"> {{challenge.number}} </div>
+                        <div class="challenge-title text-8xl font-bold capitalize text-white"> {{ challenge.title }} </div>
+                        <div class="challenge-solution text-slate-100"> {{ challenge.description }} </div>
+                    </div>
+               </template>
+               </TransitionGroup>
+                <div class="challenges flex space-x-8 items-end justify-end flex-1 pb-4 translate-x-16">
+                    <div class=""
+                     v-for="(challenge, i) in challenges"
+                     @click="handleSlides(i)"
+                     :key="challenge.id">
+                        <div class="challenge-card h-80 w-52 rounded-xl flex relative items-end shadow-lg shadow-black/70 cursor-pointer" v-if="i != active">
+                            <div class="absolute bg-cover bg-center w-full h-full brightness-75 " :class="challenge.image"></div>
+                            <div  class="absolute bg-cover bg-center w-full h-full brightness-75 " :class="`${challenge.image} bg-image-${challenge.id}`"></div>
+                            <div class="text-white z-10 p-3 space-y-2">
+                                <div class="challenge--card__line h-1 w-6 bg-white" ></div>
+                                <div class="challenge--card__number text-sm">{{ challenge.number }}</div>
+                                <div class="challenge--card__title capitalize font-bold text-2xl">{{ challenge.title }}</div>
+                            </div>
+                        </div>
+                    </div>
+                   
                 </div>
             </div>
 
